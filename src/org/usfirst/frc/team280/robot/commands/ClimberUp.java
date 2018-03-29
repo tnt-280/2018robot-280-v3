afcc7ea8ac5cc82c08d34f957d634908c33da40c
TnT 280 Programming <nathan@infiniit.co>
2018-03-28 22:34:02 -0400

    Added Climber and auton paths

---
+++ src/org/usfirst/frc/team280/robot/commands/ClimberUp.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team280.robot.commands;
+
+import org.usfirst.frc.team280.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ClimberUp extends Command {
+
+	@Override
+	protected void initialize() {
+		Robot.climber.move(0.5);
+	}
+	
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		Robot.climber.move(0);
+	}
+}
+
