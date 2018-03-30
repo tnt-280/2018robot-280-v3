a925b14083b1cbc90074ed5694ff8c06c3853b83
TnT 280 Programming <nathan@infiniit.co>
2018-03-29 20:11:09 -0400

    Changed climb down code

--- src/org/usfirst/frc/team280/robot/commands/ClimberDown.java
+++ src/org/usfirst/frc/team280/robot/commands/ClimberDown.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team280.robot.commands;
 
+import org.usfirst.frc.team280.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,30 +9,20 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class ClimberDown extends Command {
 
-    public ClimberDown() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	@Override
+	protected void initialize() {
+		Robot.climber.move(-0.5);
+	}
+	
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	@Override
+	protected void end() {
+		Robot.climber.move(0);
+	}
 }
